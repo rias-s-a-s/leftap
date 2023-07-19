fbbf4241fdaeca2fdc874ca5fcb1eef87e131cee
brahian351 <brahiangamer@gmail.com>
2023-07-19 10:41:31 -0500

    cambios previos reunion

--- app/Footer.js
+++ app/Footer.js
@@ -16,8 +16,18 @@ const Footer = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
                     <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0l-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z" />
                   </svg>
-                  <Link href={"tel:+5493815210268"} className="text-[#182d57] cursor-pointer hover:text-[#3760b1] md:text-sm">
-                    +54 9 381 521-0268
+                  <Link href={"tel:+543815210268"} className="text-[#182d57] cursor-pointer hover:text-[#3760b1] md:text-sm">
+                    +54 381 521-0268
+                  </Link>
+                </div>
+              </li>
+              <li className="mt-3">
+                <div className="flex">
+                  <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M20.25 3.75v4.5m0-4.5h-4.5m4.5 0l-6 6m3 12c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 014.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 00-.38 1.21 12.035 12.035 0 007.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 011.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 01-2.25 2.25h-2.25z" />
+                  </svg>
+                  <Link href={"tel:+543814678623"} className="text-[#182d57] cursor-pointer hover:text-[#3760b1] md:text-sm">
+                    +54 381 467-8623
                   </Link>
                 </div>
               </li>
@@ -26,8 +36,8 @@ const Footer = () => {
                   <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} stroke="currentColor" className="w-6 h-6 mr-2">
                     <path strokeLinecap="round" d="M16.5 12a4.5 4.5 0 11-9 0 4.5 4.5 0 019 0zm0 0c0 1.657 1.007 3 2.25 3S21 13.657 21 12a9 9 0 10-2.636 6.364M16.5 12V8.25" />
                   </svg>
-                  <Link href="mailto:" className="text-[#182d57] cursor-pointer hover:text-[#3760b1] md:text-sm">
-                    correo@gmail.com
+                  <Link href="mailto:informes@leftap.org" className="text-[#182d57] cursor-pointer hover:text-[#3760b1] md:text-sm">
+                    informes@leftap.org
                   </Link>
                 </div>
               </li>
@@ -45,7 +55,7 @@ const Footer = () => {
                     <path strokeLinecap="round" strokeLinejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1115 0z" />
                   </svg>
                   <Link href={"#"} className="text-[#182d57] cursor-pointer hover:text-[#3760b1] md:text-sm">
-                    Contact Support
+                    Barrio 100 Vivienda Aguirre- Lules, Tucumán Argentina
                   </Link>
                 </div>
               </li>
@@ -63,9 +73,15 @@ const Footer = () => {
             <nav className="mb-10 list-none">
               <li className="mt-3">
                 <div className="flex space-x-2 items-center">
-                  <BsFacebook className="h-6 w-6" fill="blue" />
-                  <BsInstagram className="h-6 w-6" fill="#e1306c" />
-                  <BsTiktok className="h-6 w-6" fill="black" />
+                  <BsFacebook className="h-8 w-8 lg:h-12 lg:w-12 cursor-pointer" fill="blue" onClick={() => {
+                    window.open("https://www.facebook.com/LEFTAP", "_blank")
+                  }} />
+                  <BsInstagram className="h-8 w-8 lg:h-12 lg:w-12 cursor-pointer" fill="#e1306c" onClick={() => {
+                    window.open("https://www.instagram.com/leftap.ok/", "_blank")
+                  }} />
+                  <BsTiktok className="h-8 w-8 lg:h-12 lg:w-12 cursor-pointer" fill="black" onClick={() => {
+                    window.open("https://www.tiktok.com/@leftap", "_blank")
+                  }} />
                 </div>
               </li>
             </nav>
