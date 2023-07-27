e895dc7e73b78cfbe54891371bba7c3b912ec222
brahian351 <brahiangamer@gmail.com>
2023-07-27 10:45:53 -0500

    cambios solicitados por marina

--- app/Nosotros/ModalInfo.js
+++ app/Nosotros/ModalInfo.js
@@ -4,9 +4,16 @@ function ModalInfo({ titulo, info, setOpen, Open }) {
     return (
         <>
             <div className='w-full fixed top-0 z-40 h-screen bg-black/20 text-center'>
+                <div className='text-red-700 w-full mx-auto font-bold font-[oswald] text-6xl drop-shadow-lg italic tracking-[7px] mt-14 fixed justify-center z-50 flex uppercase'>
+                    {titulo}
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={3} stroke="currentColor" className="w-8 h-8 ml-6 cursor-pointer" onClick={() => {
+                        setOpen(!setOpen)
+                    }}>
+                        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18L18 6M6 6l12 12" />
+                    </svg>
+                </div>
                 <div className='bg-white w-3/4 z-50 mx-auto mt-[35%] md:mt-[10%] h-3/4 text-center p-4 rounded-md '>
-                    <div className='font-bold p-4 rounded-md text-4xl text-white bg-blue-700'>{titulo}</div>
-                    <div className='p-2 h-3/4 text-xl md:text-2xl text-gray-800 font-serif text-justify overflow-y-scroll overflow-auto'>{info}</div>
+                    <div className='p-2 h-5/6 text-xl md:text-2xl text-gray-800 font-serif text-justify overflow-auto'>{info}</div>
                     <div className='text-center p-2'>
                         <button className='text-white font-bold bg-blue-700 rounded-md p-2 text-2xl' onClick={() => setOpen(!Open)}>Cerrar</button>
                     </div>
